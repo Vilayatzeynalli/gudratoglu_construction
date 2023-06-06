96b5db7ac9ba85a19be89fb6c34bb9ed027244c6
Vilayatzeynalli <7penukw@code.edu.az>
2023-06-06 22:05:37 +0400

    gudratoglu construction project

--- Js/index.js
+++ Js/index.js
@@ -5,79 +5,81 @@ const sortBtn=document.querySelector("#sortBtn");
 const loadMore=document.querySelector("#loadMore");
 let searchData=[];
 let allData=[];
-let maxLength=3;
+let maxLength=4;
 
-function getAllData(array){
+function createCard(arr){
     row.innerHTML="";
-    array.forEach(element => {
+    arr.forEach(element => {
         row.innerHTML+=`
         <div class="col-12 col-lg-3 mb-4 text-center">
-        <div class="card" style="width: 18rem;">
-            <img src="${element.photo}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${element.price} AZN</h5>
-              <p class="card-text">${element.address}</p>
-              <p>${element.grass} grassy</p><p>${element.square} Kv.m</p><span>${element.city}</span><br>
-              <a href="#" class="btn"><i class="fa-regular fa-heart"></i></a>
-            </div>
-          </div>
-      </div>`
+//         <div class="card" style="width: 18rem;">
+//             <img src="${element.photo}" class="card-img-top" alt="...">
+//             <div class="card-body">
+//               <h5 class="card-title">${element.price} AZN</h5>
+//               <p class="card-text">${element.address}</p>
+//               <p>${element.grass} grassy</p><p>${element.square} Kv.m</p><span>${element.city}</span><br>
+//               <a href="#" class="btn"><i class="fa-regular fa-heart"></i></a>
+//             </div>
+//           </div>
+//       </div>
+        `
     });
 }
-axios(BASE).then((res)=>getAllData(res.data));
+async function getAllData(){
+    const res=await axios(BASE);
+    let data=res.data;
+    allData=data;
+    searchData=inputSearch.value ? searchData : allData;
+    createCard(searchData.slice(0,maxLength));
+}
+getAllData();
 
-//search
-inputSearch.addEventListener("input",function(e){
-    axios(BASE).then((res)=>{
-        let dataFilter=res.data.filter((item)=>item.address
-        .toLocaleLowerCase()
-        .includes(e.target.value.toLocaleLowerCase()));
-        getAllData(dataFilter);
-    });
-});
 
-//sort
+
+loadMore.addEventListener("click",function(){
+    maxLength+=4
+    if(maxLength >= searchData.length){
+        loadMore.style.display=`none`
+
+    }if(searchData.length){
+    createCard(searchData.slice(0,maxLength));
+        
+    }
+    else{
+        getAllData()
+
+    }
+})
+
 sortBtn.addEventListener("click",function(){
-    if(this.innerHTML=="Ascending"){
-    axios(BASE).then((res)=>{
-    const sortAsc=res.data.sort((a,b)=>a.price-b.price);
-    getAllData(sortAsc);
-        });
-    this.innerHTML="Descending"; 
-    }else if(this.innerHTML=="Descending"){
-        axios(BASE).then((res)=>{
-            const sortAsc=res.data.sort((a,b)=>b.price-a.price);
-            getAllData(sortAsc);
-        });
-        this.innerHTML="Default";    
-        }else{
-            axios(BASE).then((res)=>{
-                getAllData(res.data);
-            });
-            this.innerHTML="Ascending";
-        };
-});
+    if(sortBtn.innerHTML==`ascending`){
+        searchData=searchData.sort((a,b)=>a.price-b.price)
+        sortBtn.innerHTML=`descendig`
+    }else if(sortBtn.innerHTML==`descendig`){
+        searchData=searchData.sort((a,b)=>b.price-a.price)
+        sortBtn.innerHTML=`default`
+    }else{
+        getAllData()
+        sortBtn.innerHTML=`ascending`
+    }
+    createCard(searchData.slice(0,maxLength))
+ })
 
 
-// loadMore.addEventListener("click",function(){
-//     maxLength+=3
-//     if(maxLength >= searchData.length){
-//         loadMore.style.display=`none`
+inputSearch.addEventListener("input",function(e){
+    searchData=allData;
+    searchData=searchData.filter((element)=>element.address.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase()))
+createCard(searchData.slice(0,maxLength))
+ })
 
-//     }if(searchData.length){
-//     getAllData(searchData.slice(0,maxLength));
-        
-//     }
-//     else{
-        
 
-//     }
+
+
+// window.onscroll = function () { scrollFunction() }
+// goTop.addEventListener("click",function(){
+//     document.body.scrollTop=0
+//     document.documentElement.scrollTop=0
 // })
-
-
-
-
-
 
 
 
