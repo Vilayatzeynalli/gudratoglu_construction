4c459991332a4eef7ff9c11b4d834aeb74515e27
Vilayatzeynalli <7penukw@code.edu.az>
2023-06-07 13:19:22 +0400

    gudratoglu construction project

--- Js/add.js
+++ Js/add.js
@@ -10,24 +10,24 @@ const addBtn=document.querySelector("#addBtn");
 let BASE="http://localhost:8000/apartments";
 
 
-form.addEventListener("submit",function(event){
-    event.preventDefault();
-    let cards={
-        id:Date.now(),
-        photo:inputFile.value,
-        price:inputNumber.value,
-        address:inputAddress.value,
-        grass:inputGrassy.value,
-        square:inputSquare.value,
-        city:inputCity.value
-    }
-    fetch(`http://localhost:8000/apartments`,{
-       method:"POST",
-       headers:{
-        "Content-Type":"application/json",
-       },
-       body: JSON.stringify(cards), 
-    })
-    window.location.href="./index.html"
-});
+// form.addEventListener("submit",function(event){
+//     event.preventDefault();
+//     let cards={
+//         id:Date.now(),
+//         photo:inputFile.value,
+//         price:inputNumber.value,
+//         address:inputAddress.value,
+//         grass:inputGrassy.value,
+//         square:inputSquare.value,
+//         city:inputCity.value
+//     }
+//     fetch(`http://localhost:8000/apartments`,{
+//        method:"POST",
+//        headers:{
+//         "Content-Type":"application/json",
+//        },
+//        body: JSON.stringify(cards), 
+//     })
+//     window.location.href="./index.html"
+// });
 
