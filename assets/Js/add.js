fbd6131946406ad796d456ceb21b555b810a7ec7
Vilayatzeynalli <7penukw@code.edu.az>
2023-06-15 16:32:56 +0400

    admin dashboard add font family

--- assets/Js/add.js
+++ assets/Js/add.js
@@ -72,3 +72,15 @@ form.addEventListener("submit",function(e){
 //     window.location.href="../assets/project.html"
 // });
 
+
+//menu iconu
+let menuIcon = document.querySelector("#menu");
+let navMobile = document.querySelector(".nav-mobile");
+
+menuIcon.addEventListener("click", function () {
+  navMobile.classList.toggle("show");
+
+  this.classList.contains("fa-bars")
+    ? (this.classList = "fa-solid fa-xmark")
+    : (this.classList = "fa-solid fa-bars");
+});
