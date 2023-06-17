98983f0c7c2c54e8748741c823d68f16a5f23b57
Vilayatzeynalli <7penukw@code.edu.az>
2023-06-17 11:08:51 +0400

    I adjusted the fonts

--- assets/Js/fav.js
+++ assets/Js/fav.js
@@ -21,3 +21,35 @@ async function getAllData(){
     });
 }
 getAllData();
+
+//menu iconu
+let menuIcon = document.querySelector("#menu");
+let navMobile = document.querySelector(".nav-mobile");
+
+menuIcon.addEventListener("click", function () {
+  navMobile.classList.toggle("show");
+
+  this.classList.contains("fa-bars")
+    ? (this.classList = "fa-solid fa-xmark")
+    : (this.classList = "fa-solid fa-bars");
+});
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
