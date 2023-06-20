6d43845dca37a6a0263491f97e0025539581d85f
Vilayatzeynalli <7penukw@code.edu.az>
2023-06-20 11:07:18 +0400

    I completely fixed the admin login

--- assets/Js/admin.js
+++ assets/Js/admin.js
@@ -1,39 +1,15 @@
-const form3=document.querySelector("form");
-const inputFirstName=document.querySelector("#inputFirstName");
-const inputPassword=document.querySelector("#inputPassword");
-const submitBtn=document.querySelector("#submitBtn");
+const form=document.querySelector("form");
+const userName=document.querySelector("#userName");
+const password=document.querySelector("#password");
 
-const admin_url="http://localhost:8000/users";
+const admin_url="http://localhost:8000/admin";
 
-form3.addEventListener("submit",async function(e){
-e.preventDefault();
-const res=await axios(admin_url)
-const data=await res.data
-console.log(data);
-if(data.find((item)=>item.name == inputFirstName.value && item.password == inputPassword.value)){
-    window.location="admin-dashboard.html";
-}else{
-    alert("Such user is not exists")
-}
+
+form.addEventListener("submit",function(e){
+  e.preventDefault();
+  if(userName.value==`Zeynalli` && password.value == `vilayat0111`){
+    window.location=`../admin-dashboard.html`
+  }else{
+    alert("Go away, this is not you place");
+  }
 })
-
-
-
-
-
-
-
-// let form = document.querySelector("#form");
-// let userName = document.querySelector("#username");
-// let password = document.querySelector("#password");
-// const  admin_url= "http://localhost:8000/admin";
-
-
-// form.addEventListener("submit",function(e){
-//   e.preventDefault();
-//   if(userName.value==`Zeynalli` && password.value == `vilayat0111`){
-//     window.location=`../admin-dashboard.html`
-//   }else{
-//     alert("Qaqa get sora gel");
-//   }
-// })
